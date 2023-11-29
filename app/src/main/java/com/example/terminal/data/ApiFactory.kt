0f7a280d5c3f5122b698b3dea116b9c420e2000c
= <=>
2023-11-29 21:56:34 +0700

    Пофиксил запрос в api

--- app/src/main/java/com/example/terminal/data/ApiFactory.kt
+++ app/src/main/java/com/example/terminal/data/ApiFactory.kt
@@ -1,5 +1,7 @@
 package com.example.terminal.data
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -8,8 +10,15 @@ object ApiFactory {
 
     private const val BASE_URL = "https://api.polygon.io/v2/"
 
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
--- app/src/main/java/com/example/terminal/data/BarApi.kt
+++ app/src/main/java/com/example/terminal/data/BarApi.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 
 interface BarApi {
 
-    @GET("/aggs/ticker/AAPL/range/1/hour/2023-01-09/2023-01-09?adjusted=true&sort=asc&limit=50000&apiKey=LQ8xY1Y2WDUiYDQa06HpdbOusdqyR84X")
+    @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50000&apiKey=LQ8xY1Y2WDUiYDQa06HpdbOusdqyR84X")
     suspend fun getBars(): BarResponse
 }
